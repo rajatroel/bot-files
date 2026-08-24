a35572cf114097e8adf7b21e8c770a628c33859a
rajatroel <imrajatroel@gmail.com>
2026-08-24 14:35:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ while [ ! -d "$HOME/storage/shared" ]; do
     echo -n "Waiting for you to click 'Allow'"
     
     # Check automatically every 1 second (up to 15 seconds)
-    for i in {1..15}; do
+    for i in {1..7}; do
         if [ -d "$HOME/storage/shared" ]; then
             break # Instantly break the timer if granted!
         fi
@@ -31,14 +31,14 @@ while [ ! -d "$HOME/storage/shared" ]; do
     # If the folder still isn't readable, they likely clicked Deny
     if [ ! -d "$HOME/storage/shared" ]; then
         echo ""
-        echo "❌ Storage permission not detected!"
+        echo "Storage permission not detected!"
         echo "If you clicked 'Deny', we must try again."
         echo "========================================"
         sleep 2
     fi
 done
 
-echo "✅ Storage permission granted!"
+echo "Storage permission granted!"
 sleep 1
 clear
 
